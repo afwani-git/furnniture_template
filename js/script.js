3d75af88b1b28d6b9952c2d92b53382f314f7212
afwani shofiyulloh <afwani123@gmail.com>
2019-09-22 10:05:00 +0700

    write bucket component

--- js/script.js
+++ js/script.js
@@ -1,14 +1,15 @@
-function init(){
-    let productDetail = {title: "",img: "",price: 0,qty:0};let chart = {items:[]}
-    $(window).on("load",function(){
-        try{
+function init() {
+    let productDetail = { title: "", img: "", price: 0, qty: 0 };
+    let chart = { items: [] }
+    $(window).on("load", function() {
+        try {
             let q = JSON.parse(localStorage.chart);
-            $("#bucket").find("span").text(q.items.map(prod => prod.qty).reduce((prevQty,qty) => prevQty + qty,0));
-        }catch{
+            $("#bucket").find("span").text(q.items.map(prod => prod.qty).reduce((prevQty, qty) => prevQty + qty, 0));
+        } catch {
             return false;
         }
     })
-    $(function(){
+    $(function() {
         $("#coursel-container").owlCarousel({
             items: 1
         });
@@ -30,11 +31,6 @@ function init(){
         });
     })
     $(function() {
-        $("#menu").on("click", function() {
-            $(".menu-container").slideToggle().css("display", "flex");
-        });
-    })
-    $(function() {
         $("#search").on("click", function(e) {
             e.preventDefault();
             $(".search").slideToggle().css("display", "flex");
@@ -49,54 +45,60 @@ function init(){
     $(function() {
         $(".form-qty").on("submit", function(evt) {
             evt.preventDefault();
-            try{
+            try {
                 let Jchart = "";
                 product = JSON.parse(localStorage.getItem("product"));
                 getChart = JSON.parse(localStorage.getItem("chart"));
-                if(getChart === null){
+                if (getChart === null) {
                     product.qty = parseInt($(this).find(".input-qty").prop("value"));
                     chart.items.push(product);
-                    Jchart  = JSON.stringify(chart);
-                    localStorage.setItem("chart",Jchart);
-                }else{
-                    chart =  JSON.parse(localStorage.chart);
-                    if(!chart.items.find(prod => prod.title === product.title)){
+                    Jchart = JSON.stringify(chart);
+                    localStorage.setItem("chart", Jchart);
+                } else {
+                    chart = JSON.parse(localStorage.chart);
+                    if (!chart.items.find(prod => prod.title === product.title)) {
                         product.qty = parseInt($(this).find(".input-qty").prop("value"));
                         chart.items.push(product);
                         Jchart = JSON.stringify(chart);
-                        localStorage.setItem("chart",Jchart);
-                    }else{
+                        localStorage.setItem("chart", Jchart);
+                    } else {
                         let qty = parseInt($(this).find(".input-qty").prop("value"));
                         chart.items.map(prod => {
-                            if(prod.title === product.title){
+                            if (prod.title === product.title) {
                                 prod.qty += qty
                             }
                         })
                         Jchart = JSON.stringify(chart);
-                        localStorage.setItem("chart",Jchart);
+                        localStorage.setItem("chart", Jchart);
                     }
                 }
                 let q = JSON.parse(localStorage.chart);
-                $("#bucket").find("span").text(q.items.map(prod => prod.qty).reduce((prevQty,qty) => prevQty + qty,0));
-                $("#bucket").addClass('animated bounce');$("#bucket a  path.cls-1")[0].classList.add("active-path");
+                $("#bucket").find("span").text(q.items.map(prod => prod.qty).reduce((prevQty, qty) => prevQty + qty, 0));
+                $("#bucket").addClass('animated bounce');
+                $("#bucket a  path.cls-1")[0].classList.add("active-path");
                 setTimeout(x => {
                     $("#bucket").removeClass('animated bounce');
                     $("#bucket a  path.cls-1")[0].classList.remove("active-path");
-                },900)
-            }catch{
+                }, 900)
+            } catch {
                 return false;
             }
         });
     })
     $(function() {
-        $(".box").on("click",function(evt){
+        $(".box").on("click", function(evt) {
             evt.preventDefault();
-            productDetail.title  = $(this).find(".title-info").text();
+            productDetail.title = $(this).find(".title-info").text();
             productDetail.price = parseInt($(this).find(".price-info").text().substring(1));
             productDetail.img = $(this).find('img.img-responsive').prop("src");
-            localStorage.setItem("product",JSON.stringify(productDetail));
+            localStorage.setItem("product", JSON.stringify(productDetail));
         })
     })
 }
+$(function() {
+    $("#menu").on("click", function(evt) {
+        $(".menu-container").slideToggle().css("display", "flex");
+    });
+})
 init();
 document.addEventListener('swup:contentReplaced', init);
